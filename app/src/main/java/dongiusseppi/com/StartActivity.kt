2e40d095a8d72ff5294e3734a906cd8e03b67fa9
Cesar Brandon <cesarbrandon000@gmail.com>
2023-12-29 12:41:39 -0500

    🦆 Home

--- app/src/main/java/dongiusseppi/com/StartActivity.kt
+++ app/src/main/java/dongiusseppi/com/StartActivity.kt
@@ -1,5 +1,6 @@
 package dongiusseppi.com
 
+import android.content.Intent
 import android.os.Bundle
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
@@ -8,6 +9,7 @@ import androidx.viewpager.widget.ViewPager
 import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
+import android.widget.Button
 import dongiusseppi.com.ui.main.SectionsPagerAdapter
 import dongiusseppi.com.databinding.ActivityStartBinding
 
@@ -26,5 +28,10 @@ class StartActivity : AppCompatActivity() {
         viewPager.adapter = sectionsPagerAdapter
         val tabs: TabLayout = binding.tabs
         tabs.setupWithViewPager(viewPager)
+
+        var btnIngresar = findViewById<Button>(R.id.btnIngresar)
+        btnIngresar.setOnClickListener{
+            startActivity(Intent(this, MainActivity::class.java))
+        }
     }
 }
