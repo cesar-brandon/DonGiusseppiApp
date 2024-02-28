4d6d81f13a9d61514513774abd01eb186fc81d8f
USER <cesarbrandon000@gmail.com>
2024-02-28 17:11:30 -0500

    💄 Fragment and images from categories

--- app/src/main/java/com/example/dongiusseppiapp/ui/category/CategoryFragment.kt
+++ app/src/main/java/com/example/dongiusseppiapp/ui/category/CategoryFragment.kt
@@ -7,6 +7,10 @@ import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.example.dongiusseppiapp.databinding.FragmentCategoryBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class CategoryFragment : Fragment() {
@@ -20,5 +24,19 @@ class CategoryFragment : Fragment() {
     ): View? {
         _binding = FragmentCategoryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        CoroutineScope(Dispatchers.Main).launch {
+            delay(1000)
+            binding.lottieLoading.visibility = View.INVISIBLE
+
+            binding.ivCategoryPizza.visibility = View.VISIBLE
+            binding.ivCategoryDessert.visibility = View.VISIBLE
+            binding.ivCategoryIcecream.visibility = View.VISIBLE
+            binding.ivCategorySoda.visibility = View.VISIBLE
+        }
     }
 }
